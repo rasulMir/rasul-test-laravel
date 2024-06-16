e52bc992c830ec5bc47807dd1991a0e83c567dad
rasulMyr <rasulmyrzakulov52@gmail.com>
2024-06-16 21:51:38 +0500

    # PlatformProvider # Удалил лишние ссылки
    # platform config
    # главную страницу изменил на страницу постов
    
    # platform routes
    # удалил роуты примеров

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -36,12 +36,12 @@ class PlatformProvider extends OrchidServiceProvider
         return [
 
             // post
-            Menu::make('Все посты')
-                ->title('Посты')
+            Menu::make(__('Все посты'))
+                ->title(__('Посты'))
                 ->icon('bs.stickies')
                 ->route('platform.posts.index')
                 ->active('platform.posts.index'),
-            Menu::make('Создание постов')
+            Menu::make(__('Создание постов'))
                 ->icon('bs.sticky')
                 ->route('platform.posts.create')
                 ->active('platform.posts.create')
@@ -49,68 +49,24 @@ class PlatformProvider extends OrchidServiceProvider
             // post
 
             // post-tag
-            Menu::make('Тэги')
+            Menu::make(__('Тэги'))
+                ->title(__('Тэги'))
                 ->icon('bs.tags')
                 ->route('platform.tags.index')
                 ->active('platform.tags.index'),
             // post-tag
 
-
-            Menu::make('Get Started')
-                ->icon('bs.book')
-                ->title('Navigation')
-                ->route(config('platform.index')),
-
-            Menu::make('Sample Screen')
-                ->icon('bs.collection')
-                ->route('platform.example')
-                ->badge(fn () => 6),
-
-            Menu::make('Form Elements')
-                ->icon('bs.card-list')
-                ->route('platform.example.fields')
-                ->active('*/examples/form/*'),
-
-            Menu::make('Overview Layouts')
-                ->icon('bs.window-sidebar')
-                ->route('platform.example.layouts'),
-
-            Menu::make('Grid System')
-                ->icon('bs.columns-gap')
-                ->route('platform.example.grid'),
-
-            Menu::make('Charts')
-                ->icon('bs.bar-chart')
-                ->route('platform.example.charts'),
-
-            Menu::make('Cards')
-                ->icon('bs.card-text')
-                ->route('platform.example.cards')
-                ->divider(),
-
             Menu::make(__('Users'))
                 ->icon('bs.people')
                 ->route('platform.systems.users')
                 ->permission('platform.systems.users')
-                ->title(__('Access Controls')),
+                ->title(__('Контроль доступа')),
 
             Menu::make(__('Roles'))
                 ->icon('bs.shield')
                 ->route('platform.systems.roles')
                 ->permission('platform.systems.roles')
                 ->divider(),
-
-            Menu::make('Documentation')
-                ->title('Docs')
-                ->icon('bs.box-arrow-up-right')
-                ->url('https://orchid.software/en/docs')
-                ->target('_blank'),
-
-            Menu::make('Changelog')
-                ->icon('bs.box-arrow-up-right')
-                ->url('https://github.com/orchidsoftware/platform/blob/master/CHANGELOG.md')
-                ->target('_blank')
-                ->badge(fn () => Dashboard::version(), Color::DARK),
         ];
     }
 
